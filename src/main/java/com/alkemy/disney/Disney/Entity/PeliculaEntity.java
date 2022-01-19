452a0cc28cbe418c337b41894cad5d2a8a5efd66
rocamacho <camachoromilda@gmail.com>
2022-01-19 15:22:30 -0300

    creacion Base de datos

--- src/main/java/com/alkemy/disney/Disney/Entity/PeliculaEntity.java
+++ src/main/java/com/alkemy/disney/Disney/Entity/PeliculaEntity.java
@@ -1,5 +1,57 @@
 package com.alkemy.disney.Disney.Entity;
+import lombok.Getter;
+import lombok.Setter;
 
-public class PeliculaEntity {
+import javax.persistence.*;
 
-}
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.*;
+
+
+
+	@Entity
+	@Table(name="peliculas")
+	@Getter
+	@Setter
+	public class PeliculaEntity{
+
+		@Id
+	    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+	    private Long id;
+		
+		private String imagen;
+		
+		private String titulo;
+		
+		@Column (name="fecha_creacion")
+		@DateTimeFormat(pattern = "yyyy/MM/dd")
+		private LocalDate fechaCreacion;
+					
+		private Long calificacion;
+		
+		@ManyToOne (fetch= FetchType.EAGER, cascade= CascadeType.ALL)
+		@JoinColumn(name="genero_id", insertable = false, updatable=false)
+		private GeneroEntity genero;
+		
+		@Column(name="genero_id", nullable=false)
+		private Long generoId;
+		
+		@ManyToMany(
+		      cascade= {
+		    		  CascadeType.PERSIST,
+		    		  CascadeType.MERGE
+		      })
+	    @JoinTable( 
+	           name= "personaje_pelicula",
+	           joinColumns=@JoinColumn(name="pelicula_id"),
+	           inverseJoinColumns= @JoinColumn(name ="personaje_id"))
+		private Set<PersonajeEntity> personajes= new HashSet<>();
+		
+	}
+		
+		
--- src/main/java/com/alkemy/disney/Disney/Entity/PersonajeEntity.java
+++ src/main/java/com/alkemy/disney/Disney/Entity/PersonajeEntity.java
@@ -1,5 +1,37 @@
 package com.alkemy.disney.Disney.Entity;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.*;
+import org.springframework.format.annotation.DateTimeFormat;
 
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Entity
+@Table(name="personaje")
+@Getter
+@Setter
 public class PersonajeEntity {
+
+	  
+	@Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+	
+	private String imagen;
+	
+	private String nombre;
+	private Long edad;
+	private String peso;
+	private String historia;
+	
+	private boolean deleted= Boolean.FALSE;
+	
+	@ManyToMany(mappedBy="personajes", cascade=CascadeType.ALL)
+	private List<PeliculaEntity> peliculas =new ArrayList<>();
+	
+	
 
 }
